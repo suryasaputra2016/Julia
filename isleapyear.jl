dbc2fc954b01ed4acae9b9e0f4df9ccdf572cf52
Surya Saputra <sayasuryasaputra@gmail.com>
2021-11-20 03:44:58 +0700

    DNA counting

--- isleapyear.jl
+++ isleapyear.jl
@@ -1,6 +1,8 @@
+
+
 function isleapyear(year::Int)
-    isdivisible(m, by) = m % by == 0
-    if isdivisible(year, by=400) || ( !(isdivisible(year, by=100)) && isdivisible(year, by=4) )
+    isdivisible(p, q) = p % q == 0
+    if isdivisible(year, 400) || ( !(isdivisible(year, 100)) && isdivisible(year, 4) )
         return true
     end
     return false
